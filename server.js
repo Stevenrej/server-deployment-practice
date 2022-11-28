dcdb8b111ba25699411a1ad327494b7ce70b1c3f
Stevenrej <steveo732@gmail.com>
2022-11-28 16:40:19 -0600

    added error handling

--- server.js
+++ server.js
@@ -4,15 +4,25 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 3002;
+const logger = require('./middleware/logger');
+const notFound = require('./handlers/404');
+const errorHandler = require('./handlers/500');
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+// app.use(logger);
 
-app.get('/', (req, res, next) => {
+app.get('/', logger, (req, res, next) => {
   res.status(200).send('Howdy Yall!!!!!');
 });
+
+app.get('/bad',  (req, res, next) => {
+  next('OH NO ERROR');
+});
+
+app.use('*', notFound);
+app.use(errorHandler);
 
 function start() {
   app.listen(PORT, () => console.log(`listening on ${PORT}`));
